8d596ee76a1e0f8ae76acb560e7dabf2c4e8ccd2
Danilo Alves <daniloalves@aluno.fapce.edu.br>
2020-09-28 23:24:31 -0300

    tentando retomar os testes pela milésima vez

--- src/App.js
+++ src/App.js
@@ -5,20 +5,8 @@ import Record from './components/Record';
 export default function App() {
   return (
     <>
-      {false && (
-        <div>
-          <h3>
-            Mudei o acesso à fila (de novo rs). Entre em{' '}
-            <a href={'http://192.168.1.200:3000'}>http://192.168.1.200:3000</a>
-          </h3>
-        </div>
-      )}
-      {true && (
-        <div>
-          <Record />
-          <ServiceScreen />
-        </div>
-      )}
+      <Record />
+      <ServiceScreen />
     </>
   );
 }
